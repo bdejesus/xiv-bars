05d32a4089721d58f612864f746bb05b978c358b
Ben <dejesus.ben@gmail.com>
2019-03-02 13:59:41 -0800

    Setting up css modules

--- src/App.js
+++ src/App.js
@@ -4,13 +4,23 @@ import styles from './styles.scss';
 class App extends Component {
   render() {
     return (
-      <main className="App">
+      <main className="app">
         <div className='container'>
           <h1>Hello World!</h1>
 
           <div className={styles.xbar}>
-            <div className='slot'> 
-              1
+            <div className={styles.xbarGroup}>
+              <div className={styles.slot}> 1</div>
+              <div className={styles.slot}> 2</div>
+              <div className={styles.slot}> 3</div>
+              <div className={styles.slot}> 4</div>
+            </div>
+
+            <div className={styles.xbarGroup}>
+              <div className={styles.slot}> 1</div>
+              <div className={styles.slot}> 2</div>
+              <div className={styles.slot}> 3</div>
+              <div className={styles.slot}> 4</div>
             </div>
           </div>
         </div>
