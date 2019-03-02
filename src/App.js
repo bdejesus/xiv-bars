8a50c9603db5b9b355aa5a896f121db95abea94b
Ben <dejesus.ben@gmail.com>
2019-03-02 13:37:22 -0800

    Styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import styles from './styles.scss';
 
 class App extends Component {
   render() {
     return (
       <main className="App">
-        <h1>Hello World!</h1>
+        <div className='container'>
+          <h1>Hello World!</h1>
+
+          <div className={styles.xbar}>
+            <div className='slot'> 
+              1
+            </div>
+          </div>
+        </div>
       </main>
     );
   }
