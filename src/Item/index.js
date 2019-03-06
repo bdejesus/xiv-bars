c4dc7916c995513cd6dd683963d8217e3f1deafb
Ben <dejesus.ben@gmail.com>
2019-03-06 08:12:56 -0800

    Adds item link

--- src/Item/index.js
+++ src/Item/index.js
@@ -19,7 +19,12 @@ class Item extends Component {
         draggable 
         onDragStart={() => {this.props.dragged(this.props.action)}}
       >
-        { this.props.action.Name }
+        <a 
+          href={`https://na.finalfantasyxiv.com/lodestone/playguide/db/item/b2cab67854f/`} 
+          className="eorzeadb_link"
+        >
+          { this.props.action.Name }
+        </a>
       </div>
     );
   }
