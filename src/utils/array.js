4a2a5be5f6474b116669925604ea08a5537b6fe9
Benedicto de Jesus <ben@strava.com>
2019-04-18 17:17:34 -0700

    Adds object sorting method for utils/array

--- src/utils/array.js
+++ src/utils/array.js
@@ -7,4 +7,27 @@ export const group = (array, size) => {
   return newArray;
 };
 
-export default group;
+export const compare = (key, order = 'asc') => (a, b) => {
+  // eslint-disable-next-line no-prototype-builtins
+  if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+    return 0;
+  }
+
+  const varA = (typeof a[key] === 'string') ? a[key].toUpperCase() : a[key];
+  const varB = (typeof b[key] === 'string') ? b[key].toUpperCase() : b[key];
+
+  let comparison = 0;
+  if (varA > varB) {
+    comparison = 1;
+  } else if (varA < varB) {
+    comparison = -1;
+  }
+  return (
+    (order === 'desc') ? (comparison * -1) : comparison
+  );
+};
+
+export default {
+  compare,
+  group
+};
