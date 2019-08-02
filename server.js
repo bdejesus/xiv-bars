fabccc50881bf9547239e2c87aeffc826b9b6833
Ben de Jesus <dejesus.ben@gmail.com>
2019-08-01 20:09:16 -0700

    Updates server.js to redirect http to https

--- server.js
+++ server.js
@@ -3,12 +3,20 @@ const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
+
+app.use(function (req, resp, next) {
+  if (req.headers['x-forwarded-proto'] == 'http') {
+    return resp.redirect(301, 'https://' + req.headers.host + '/');
+  } else {
+    return next();
+  }
+});
 app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/ping', function (req, res) {
- return res.send('pong');
+  return res.send('pong');
 });
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
