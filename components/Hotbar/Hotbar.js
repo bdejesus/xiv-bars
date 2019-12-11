f24ef3111f4dfc143d10fe6884d9286c971b309a
Benedicto de Jesus <dejesus.ben@gmail.com>
2019-12-11 16:08:38 -0600

    Adds Hotbar layout and siwtching

---
+++ components/Hotbar/Hotbar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Slot from 'components/Slot';
+import xBars from 'models/xbars';
+import styles from './styles.scss';
+
+function Row({ slots }) {
+  return (
+    <ol className={styles.row}>
+      {Object.keys(slots).map((slot) => (
+        <li key={`slot-${slots[slot].id}`}>
+          <Slot />
+        </li>
+      ))}
+    </ol>
+  );
+}
+
+Row.propTypes = {
+  slots: PropTypes.shape().isRequired
+};
+
+function Hotbar() {
+  const { bars } = xBars.hotbars;
+  return (
+    <ol className={styles.container}>
+      {Object.keys(bars).map((barKey) => (
+        <li key={barKey} className={styles.rowWrapper}>
+          <Row slots={bars[barKey]} />
+        </li>
+      ))}
+    </ol>
+  );
+}
+
+export default Hotbar;
--- components/Slot/Slot.js
+++ components/Slot/Slot.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import Action from 'components/Action';
 import styles from './styles.scss';
 
-function Slot({ id }) {
+function Slot({ id, className }) {
   const [dragging, setDragging] = useState(false);
   const [action, setAction] = useState({});
 
@@ -44,7 +44,7 @@ function Slot({ id }) {
   return (
     <div
       id={id}
-      className={styles.slot}
+      className={`${styles.slot} ${className}`}
       onDrop={(event) => handleDrop(event)}
       onDragStart={(event) => handleDragStart(event)}
       onDragOver={(event) => handleDragOver(event)}
@@ -59,4 +59,9 @@ export default Slot;
 
 Slot.propTypes = {
   id: PropTypes.string.isRequired,
+  className: PropTypes.string
+};
+
+Slot.defaultProps = {
+  className: ''
 };
