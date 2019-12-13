4b26d12b6b61cc04e7bb55d049b931606cf744ce
Benedicto de Jesus <dejesus.ben@gmail.com>
2019-12-13 10:29:10 -0600

    Updates General Action descriptions

--- models/actions.js
+++ models/actions.js
@@ -8,8 +8,7 @@ const generalActions = [
   {
     ID: 209,
     Name: 'Limit Break',
-    Description:
-      'Limit Break is a Light Party or Full Party (4 or 8 player) ability. It can only be used after the Limit Break Gauge has at least one segment filled. The length of the Limit Break gauge depends on the type of party',
+    Description: 'Limit Break is a Light Party or Full Party (4 or 8 player) ability. It can only be used after the Limit Break Gauge has at least one segment filled. The length of the Limit Break gauge depends on the type of party',
     Icon: '/i/000000/000103.png'
   },
   {
@@ -21,13 +20,14 @@ const generalActions = [
   {
     ID: 6,
     Name: 'Return',
-    Description: "A form of teleporting, available from the Actions & Traits Menu under the 'General' section which allows you to return to your Home Point, or the start of the Duty/Dungeon that you're currently in, once every 15 minutes for zero cost.",
+    Description: 'Transports the user to their current Aetheryte home point free of charge.',
     Icon: '/i/000000/000111.png'
   },
   {
     ID: 5,
     Name: 'Teleport',
-    Description: 'Teleporting is the quickest method of transportation in Eorzea. Once a player has attuned with an Aetheryte crystal, the player may teleport at any time (except while in combat) to that location, from anywhere.',
+    Description:
+      'Transports the user and party members to the chosen Aetheryte location for a fee. The fee is higher the further the destination.',
     Icon: '/i/000000/000112.png'
   }
 ];
