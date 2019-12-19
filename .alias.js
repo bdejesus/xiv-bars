63be6016188f748baade143cb38802a18a857975
Benedicto de Jesus <dejesus.ben@gmail.com>
2019-12-19 15:50:14 -0600

    Cleans up image folder structure

--- .alias.js
+++ .alias.js
@@ -4,7 +4,6 @@ module.exports = {
   '~': path.join(__dirname, './pages'),
   components: path.join(__dirname, './components'),
   constants: path.join(__dirname, './constants'),
-  images: path.join(__dirname, './images'),
   models: path.join(__dirname, './models'),
   utils: path.join(__dirname, './utils')
 };
