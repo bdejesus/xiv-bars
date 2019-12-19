60763009e69f223a3eef1b7d0d2dad3cbf8c17cd
Benedicto de Jesus <dejesus.ben@gmail.com>
2019-12-19 14:30:20 -0600

    Cleaning up LoadScreen component

---
+++ components/LoadScreen/LoadScreen.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import Router from 'next/router';
+
+import styles from './styles.scss';
+
+function LoadScreen() {
+  const [isLoading, setIsLoading] = useState(false);
+  Router.events.on('routeChangeStart', () => setIsLoading(true));
+  Router.events.on('routeChangeComplete', () => setIsLoading(false));
+  // eslint-disable-next-line no-console
+  Router.events.on('routeChangeError', () => console.error('error'));
+
+  return (
+    <div className={styles.container} aria-hidden={!isLoading}>
+      <div className={styles.wrapper}>
+        <img src="/mog_trumpet.png" alt="" />
+        <div className={styles.title}>Loading...</div>
+      </div>
+    </div>
+  );
+}
+
+export default LoadScreen;
