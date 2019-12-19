104488027e8dd31181f89bdec748d9c3d3efb3d4
Benedicto de Jesus <dejesus.ben@gmail.com>
2019-12-19 15:45:56 -0600

    Cleans up and optimizes css

--- .stylelint.js
+++ .stylelint.js
@@ -1,6 +1,13 @@
 module.exports = {
   extends: "stylelint-config-standard",
   rules: {
-    "selector-pseudo-class-no-unknown": [true, { ignorePseudoClasses: ['local', 'global']}]
+    "selector-pseudo-class-no-unknown": [
+      true, 
+      { ignorePseudoClasses: ['local', 'global'] }
+    ],
+    "rule-empty-line-before": [
+      "always", 
+      { except: ["inside-block"] }
+    ]
   }
 }
