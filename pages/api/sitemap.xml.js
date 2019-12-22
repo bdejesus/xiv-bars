c1f6f87115dcea8df4cc76206d7a9329574c4fd0
Benedicto de Jesus <dejesus.ben@gmail.com>
2019-12-22 14:25:25 -0600

    Adds a sitemap.xml under api

---
+++ pages/api/sitemap.xml.js
@@ -0,0 +1,35 @@
+import { SitemapStream, streamToPromise } from 'sitemap';
+import { advancedJobs } from 'models/jobs';
+
+export default async (req, res) => {
+  try {
+    const sitemap = new SitemapStream({
+      hostname: `https://${req.headers.host}`,
+      cacheTime: 600000
+    });
+
+    // Create each URL row
+    advancedJobs.forEach((job) => {
+      sitemap.write({
+        url: `/job/${job.Abbr}`,
+        priority: 0.9
+      });
+    });
+
+    // End sitemap stream
+    sitemap.end();
+
+    // XML sitemap string
+    const sitemapOutput = (await streamToPromise(sitemap)).toString();
+
+    // Change headers
+    res.writeHead(200, {
+      'Content-Type': 'application/xml'
+    });
+
+    // Display output to user
+    res.end(sitemapOutput);
+  } catch (e) {
+    res.send(JSON.stringify(e));
+  }
+};
