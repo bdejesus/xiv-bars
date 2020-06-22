7fc30aa6c87d81bc32301bda987bde2c684d855a
Benedicto de Jesus <dejesus.ben@gmail.com>
2020-06-22 09:06:09 -0700

    CSS Cleanup to move global styles to a global.scss file

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,8 @@ import App from 'next/app';
 import Head from 'next/head';
 import { Meta, Icons } from './includes';
 
+import './global.scss';
+
 class AppContainer extends App {
   render() {
     const { Component, pageProps } = this.props;
@@ -45,26 +47,6 @@ class AppContainer extends App {
             canonical={canonicalUrl}
           />
           <Icons />
-
-          {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=UA-144541753-2"
-          />
-          {/* eslint-disable-next-line react/no-danger */}
-          <script dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag() {
-                dataLayer.push(arguments);
-              }
-              gtag("js", new Date());
-              gtag("config", "UA-144541753-2");
-            `
-          }}
-          />
-          {/* Google AdSense */}
-          <script data-ad-client="ca-pub-3274093949320222" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
         </Head>
 
         <main>
