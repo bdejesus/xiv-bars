6cd5542ef7602941ed9670988f419b9fb4ed76ad
Benedicto de Jesus <dejesus.ben@gmail.com>
2020-07-06 16:33:17 -0700

    Force https

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const next = require('next');
+const secure = require('ssl-express-www');
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
@@ -8,6 +9,8 @@ const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   const server = express();
+  
+  server.use(secure);
 
   server.get('/', (req, res) => {
     app.render(req, res, '/index', req.query);
