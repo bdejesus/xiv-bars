71624a775ac73bca24357e90ef0068f558174072
Benedicto de Jesus <dejesus.ben@gmail.com>
2020-08-29 18:08:12 -0700

    Copy updates

--- components/Articles/Articles.js
+++ components/Articles/Articles.js
@@ -3,30 +3,33 @@ import React from 'react';
 function Articles() {
   return (
     <div className="container">
-      <h3>
-        <a href="https://josebenedicto.com/ffxiv/cross-hotbar-settings--auto-switching-for-battle">
-          Cross Hotbar UI Settings to Auto-switch for Combat
-        </a>
-      </h3>
+      <h2>Guides</h2>
 
-      <p>
-        How to set your Cross Hotbars in Controller Mode to auto-switch
-        to a combat hotbar when entering battle stance.
-      </p>
+      <div className="mt-md">
+        <h3>
+          <a href="https://josebenedicto.com/ffxiv/cross-hotbar-settings--auto-switching-for-battle">
+            Configure the Cross Hotbar UI to Auto-Switch for Combat
+          </a>
+        </h3>
 
-      <p>
-        If you play in <b>Controller Mode</b>, you can set your Cross
-        Hotbars to automatically switch to a different Hotbar whenever
-        you go into battle stance. This frees you up from having to
-        manually switch to the correct hotbar every time you&apos;re
-        coming in and out of combat.
-      </p>
+        <p>
+          How to configure your Cross Hotbar UI settings in Controller Mode to automatically switch between a combat and passive hotbar  when entering and exiting battle.
+        </p>
 
-      <p>
-        <a href="https://josebenedicto.com/ffxiv/cross-hotbar-settings--auto-switching-for-battle">
-          Read more...
-        </a>
-      </p>
+        <p>
+          If you play in <b>Controller Mode</b>, you can set your Cross
+          Hotbars to automatically switch to a different Hotbar whenever
+          you go into battle stance. This frees you up from having to
+          manually switch to the correct hotbar every time you&apos;re
+          coming in and out of combat.
+        </p>
+
+        <p>
+          <a href="https://josebenedicto.com/ffxiv/cross-hotbar-settings--auto-switching-for-battle">
+            Read more...
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
