2b07c43945e0383b11150b5db69d4042669eb071
Benedicto de Jesus <dejesus.ben@gmail.com>
2020-08-30 13:46:30 -0700

    Fixing backwards compatibility with action ID formats

--- components/App/context.js
+++ components/App/context.js
@@ -1,101 +1,11 @@
 import React, { createContext, useReducer } from 'react';
 import { useRouter } from 'next/router';
 import PropTypes from 'prop-types';
-import { xbars, hotbars, layouts } from 'lib/xbars';
-import ACTION_TYPE from 'lib/actionType';
-import BUDDY_ACTION from 'data/BuddyAction';
-import COMPANY_ACTION from 'data/CompanyAction';
-import GENERAL_ACTION from 'data/GeneralAction';
-import MAIN_COMMAND from 'data/MainCommand';
-import MACRO_ICON from 'data/MacroIcon';
-import PET_ACTION from 'data/PetAction';
+import { xbars, hotbars } from 'lib/xbars';
+import AppReducer from './reducers';
 
 const AppContext = createContext();
 const AppDispatchContext = createContext();
-
-function AppReducer(state, payload) {
-  const { layout } = state;
-
-  switch (payload.type) {
-    case 'updateLayout': {
-      return { ...state, layout: payload.layout };
-    }
-
-    case 'bulkLoadActionsToSlots': {
-      const { slottedActions } = payload;
-      const slots = state[layouts[layout]];
-
-      slottedActions.forEach((actionGroup, groupIndex) => {
-        const groupName = Object.keys(slots)[groupIndex];
-        const slotGroup = state[layouts[layout]][groupName];
-
-        actionGroup.forEach((actionID, slotIndex) => {
-          const prefixes = [...Object.values(ACTION_TYPE), 'r'].join('|');
-          const actionRegex = new RegExp(prefixes);
-          const actionType = actionID.match(actionRegex);
-          const parsedID = parseInt(actionID.replace(actionType, ''), 10);
-
-          const actionLib = () => {
-            if (actionType) {
-              switch (actionType.toString()) {
-                case ACTION_TYPE.BuddyAction: return BUDDY_ACTION;
-                case ACTION_TYPE.CompanyAction: return COMPANY_ACTION;
-                case ACTION_TYPE.GeneralAction: return GENERAL_ACTION;
-                case ACTION_TYPE.MainCommand: return MAIN_COMMAND;
-                case ACTION_TYPE.MacroIcon: return MACRO_ICON;
-                case ACTION_TYPE.PetAction: return PET_ACTION;
-                case 'r': return state.roleActions;
-                default: return state.actions;
-              }
-            }
-            return state.actions;
-          };
-
-          const slottedAction = actionLib().find((action) => action.ID === parsedID);
-
-          if (slottedAction) {
-            slotGroup[slotIndex].action = slottedAction;
-          }
-        });
-      });
-
-      return { ...state };
-    }
-
-    case 'setActionToSlot': {
-      // update slotted actions
-      const [parent, id] = payload.slotID.split('-');
-      const slot = { parent, id: id - 1 };
-      const slotObject = state[layouts[layout]][slot.parent][slot.id];
-      slotObject.action = payload.action;
-
-      // update slots string query
-      const stringifiedSlots = () => {
-        const slotIDs = Object.values(state[layouts[layout]]);
-        const queryString = slotIDs.map((arr) => {
-          const group = Object.values(arr).map((obj) => {
-            if (obj.action && obj.action.ID) {
-              if (typeof obj.action.Prefix !== 'undefined') {
-                return `${obj.action.Prefix}${obj.action.ID}`;
-              }
-              return `${obj.action.ID}`;
-            }
-            return '0';
-          });
-
-          return group;
-        });
-
-        return JSON.stringify(queryString);
-      };
-
-      return { ...state, encodedSlots: stringifiedSlots() };
-    }
-    default: {
-      throw new Error(`Unhandled action type: ${payload.type}`);
-    }
-  }
-}
 
 function useAppState() {
   const context = React.useContext(AppContext);
