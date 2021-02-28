f8d92cc13b1cb5ae79a59cbd487e1e0237d93a60
Benedicto de Jesus <dejesus.ben@gmail.com>
2021-02-27 19:17:04 -0800

    Toggleable modal

--- components/ExportToMacros/ExportToMacros.js
+++ components/ExportToMacros/ExportToMacros.js
@@ -1,10 +1,12 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { layouts, chotbarNames } from 'lib/xbars';
 import { useAppState } from 'components/App/context';
-import styles from './Export.module.scss';
+import styles from './ExportToMacros.module.scss';
 
 function Export() {
+  const [showModal, setShowModal] = useState(false);
   const textarea = useRef();
+  const modal = useRef();
   const appState = useAppState();
   const { layout, encodedSlots } = appState;
   const currLayout = layouts[layout];
@@ -22,6 +24,15 @@ function Export() {
       .join('\n');
   }
 
+  function selectTextarea() {
+    textarea.current.focus();
+    textarea.current.select();
+  }
+
+  function toggleModal() {
+    setShowModal(!showModal);
+  }
+
   useEffect(() => {
     const hotbarMacros = Object
       .values(appState[currLayout])
@@ -37,12 +48,23 @@ function Export() {
         type="button"
         className={styles.macroBtn}
         title="Export to Macro"
+        onClick={toggleModal}
       >
         M
       </button>
 
-      <div className={styles.modal}>
-        <textarea ref={textarea} />
+      <div
+        className={styles.modal}
+        data-active={showModal}
+        ref={modal}
+        onClick={toggleModal}
+      >
+        <div
+          className={styles.modalContent}
+          onClick={(e) => e.stopPropagation()}
+        >
+          <textarea ref={textarea} readOnly onClick={selectTextarea} />
+        </div>
       </div>
     </div>
   );
