a2ff1674ba74645807445571e9272e77fe02e1fe
Benedicto de Jesus <dejesus.ben@gmail.com>
2021-05-01 17:20:46 -0700

    Fixup to tests

--- tests/components/App/App.test.js
+++ tests/components/App/App.test.js
@@ -1,6 +1,13 @@
 import { shallow } from 'tests/enzyme';
 import App from 'components/App';
 
+jest.mock('data/GeneralAction', () => {}, { virtual: true });
+jest.mock('data/MainCommand', () => {}, { virtual: true });
+jest.mock('data/MacroIcon', () => {}, { virtual: true });
+jest.mock('data/PetAction', () => {}, { virtual: true });
+jest.mock('data/BuddyAction', () => {}, { virtual: true });
+jest.mock('data/CompanyAction', () => {}, { virtual: true });
+
 const props = {
   jobs: [],
   actions: [],
