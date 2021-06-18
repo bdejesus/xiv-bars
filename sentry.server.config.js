4ffba54f7c73597922bcce96ef5ee73157b289b9
Benedicto de Jesus <dejesus.ben@gmail.com>
2021-06-17 17:11:26 -0700

    Adds environment variables

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -5,7 +5,9 @@
 import * as Sentry from '@sentry/nextjs';
 
 const SENTRY_DSN = process.env.NEXT_PUBLIC_SENTRY_DSN;
+const ENV = process.env.NEXT_PUBLIC_ENV;
 
 Sentry.init({
-  dsn: SENTRY_DSN
+  dsn: SENTRY_DSN,
+  environment: ENV
 });
