959ff1ab110508d113a7151e60edef5ee779b5e2
Benedicto de Jesus <dejesus.ben@gmail.com>
2021-07-25 21:58:56 -0700

    Adds Eorzea Profile component

---
+++ components/EorzeaProfile/EorzeaProfile.js
@@ -0,0 +1,29 @@
+import styles from './EorzeaProfile.module.scss';
+
+function EorzeaProfile() {
+  return (
+    <a
+      href="https://dashboard.twitch.tv/extensions/bsdr8tkgubqusuao9ixx6wjyhxy8je-1.1.0"
+      target="_blank"
+      rel="noreferrer"
+      className={`${styles.container} container`}
+    >
+      <div className={styles.image}>
+        <img src="/images/ffxiv-screenshot.png" alt="" />
+      </div>
+      <div className={styles.body}>
+        <h2>FFXIV Profile Twitch Streamer Panel</h2>
+
+        <p className={styles.subtitle}>
+          Display your Final Fantasy XIV character profile on your stream page.
+        </p>
+
+        <div className={styles.cta}>
+          Install Now
+        </div>
+      </div>
+    </a>
+  );
+}
+
+export default EorzeaProfile;
