7b8f2c9c3d4021d83ce31ea58be6e251c08403d7
Benedicto de Jesus <dejesus.ben@gmail.com>
2021-07-25 22:34:59 -0700

    Upgrades Sentry and configs (#60)

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -4,10 +4,12 @@
 
 import * as Sentry from '@sentry/nextjs';
 
-const SENTRY_DSN = process.env.NEXT_PUBLIC_SENTRY_DSN;
+const SENTRY_DSN = process.env.SENTRY_DSN;
 const ENV = process.env.NEXT_PUBLIC_ENV;
 
 Sentry.init({
   dsn: SENTRY_DSN,
-  environment: ENV
+  environment: ENV,
+  enabled: process.env.NODE_ENV === 'production',
+  tracesSampleRate: 1.0
 });
