66781477367c4f61811379a0d44aa7e601520a5c
Benedicto de Jesus <dejesus.ben@gmail.com>
2021-12-09 11:41:02 -0800

    Fixup

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,14 @@
 /* eslint-disable react/no-danger */
 import React from 'react';
 import Document, {
-  Html, Main, NextScript
+  Html, Head, Main, NextScript
 } from 'next/document';
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en-US">
+        <Head />
         <body>
           <Main />
           <NextScript />
