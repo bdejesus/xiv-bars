2113b88e61ef3cdfb64a37512e7a419a60999ac0
Benedicto de Jesus <dejesus.ben@gmail.com>
2021-12-09 10:28:15 -0800

    More style cleanups

--- components/GlobalHeader/GlobalHeader.js
+++ components/GlobalHeader/GlobalHeader.js
@@ -4,16 +4,18 @@ function GlobalHeader() {
   return (
     <div className={styles.container}>
       <a href="/" className={styles.branding}>
-        <img
-          src="/icons/favicon-96x96.png"
-          alt="XIVBARS Logo"
-          height={24}
-          width={24}
-        />
-        <b className={styles.title}>XIVBARS</b>
-        <span className={styles.subTitle}>
+        <div className={styles.logo}>
+          <img
+            src="/icons/favicon-96x96.png"
+            alt="XIVBARS Logo"
+            height={24}
+            width={24}
+          />
+        </div>
+        <div className={styles.title}>XIVBARS</div>
+        <div className={styles.subtitle}>
           A Final Fantasy XIV Endwalker Hotbar Planner
-        </span>
+        </div>
       </a>
     </div>
   );
