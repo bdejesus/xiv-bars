38cfa51fb5ed88d61b06075ee265fbc8484673d5
Ben <dejesus.ben@gmail.com>
2022-06-25 16:25:16 -0700

    Fixup

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,7 +32,7 @@ module.exports = {
     'linebreak-style': ['warn', 'unix'],
     'max-len': 'off',
     'no-debugger': 'warn',
-    'no-restricted-exports': 'off',
+    'no-restricted-exports': 'warn',
     'no-unused-vars': 'warn',
     'prefer-destructuring': 'off',
     'react/destructuring-assignment': 'off',
