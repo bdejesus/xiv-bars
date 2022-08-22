f880959e080a777de06f1e8b8c9ac8b686a54f4d
Benedicto de Jesus <dejesus.ben@gmail.com>
2022-08-22 19:56:12 +0200

    Reverts gtag updates

--- pages/_app.js
+++ pages/_app.js
@@ -40,24 +40,20 @@ export function App({ Component, pageProps }) {
   return (
     <>
       {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
-      { process.env.NEXT_PUBLIC_ENV === 'production' && (
-        <>
-          <Script
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
-            strategy="afterInteractive"
-          />
-          <Script id="google-analytics" strategy="afterInteractive">
-            {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag() {
-              dataLayer.push(arguments);
-            }
-            gtag("js", new Date());
-            gtag("config", "${process.env.NEXT_PUBLIC_GA_ID}");
-          `}
-          </Script>
-        </>
-      )}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag() {
+            dataLayer.push(arguments);
+          }
+          gtag("js", new Date());
+          gtag("config", "${process.env.NEXT_PUBLIC_GA_ID}");
+        `}
+      </Script>
 
       <Head>
         <title>{title}</title>
