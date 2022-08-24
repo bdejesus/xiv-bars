c45852771f3ea5d635dd28ca959094a2bebe9bac
Benedicto de Jesus <dejesus.ben@gmail.com>
2022-08-24 12:54:20 +0200

    Refactoring Layouts Card components

--- pages/me/index.js
+++ pages/me/index.js
@@ -2,78 +2,19 @@
 import { useEffect, useState } from 'react';
 import { useSession } from 'next-auth/react';
 import { useUserDispatch } from 'components/User/context';
-import PropTypes from 'prop-types';
 import Head from 'next/head';
 import I18n from 'lib/I18n/locale/en-US';
 import fetch from 'node-fetch';
-import Link from 'next/link';
 import GlobalHeader from 'components/GlobalHeader';
 import JobMenu from 'components/JobSelect/JobMenu';
-import Job from 'components/JobSelect/Job';
+import LayoutCard from 'components/LayoutCard';
+import Card from 'components/Card';
 import Footer from 'components/Footer';
 import LoadScreen from 'components/LoadScreen';
 import { listJobs, } from 'lib/api';
 import { maxLayouts } from 'lib/user';
 
 import styles from './me.module.scss';
-
-function LayoutCard({ layout, job, onDelete }) {
-  const [showPrompt, setShowPrompt] = useState(false);
-
-  function formatDate(date) {
-    const formattedDate = new Date(date);
-    return formattedDate.toDateString();
-  }
-
-  return (
-    <>
-      <a className={styles.card} href={`/job/${layout.jobId}/${layout.id}`}>
-        <h4>{layout.title}</h4>
-        <p className={styles.description}>{layout.description}</p>
-
-        <div className={styles.cardFooter}>
-          <Job job={job} className={styles.job} />
-          <div className={styles.timestamp}>
-            {I18n.Pages.Me.last_updated}: {formatDate(layout.updatedAt)}
-          </div>
-        </div>
-      </a>
-
-      <div className={styles.actions}>
-        <button
-          type="button"
-          onClick={() => setShowPrompt(true)}
-          className={styles.removeButton}
-        >
-          Delete
-        </button>
-      </div>
-
-      { showPrompt && (
-        <div className={styles.prompt} data-active={!showPrompt}>
-          <div className={styles.promptContent}>
-            <p>Are you sure you want to delete <b>{layout.title}</b>?</p>
-            <div className={styles.promptActions}>
-              <button type="button" onClick={onDelete}>Delete</button>
-              <button
-                type="button"
-                onClick={() => setShowPrompt(false)}
-              >
-                Cancel
-              </button>
-            </div>
-          </div>
-        </div>
-      )}
-    </>
-  );
-}
-
-LayoutCard.propTypes = {
-  layout: PropTypes.shape().isRequired,
-  job: PropTypes.shape().isRequired,
-  onDelete: PropTypes.func.isRequired
-};
 
 export default function Me(pageProps) {
   const { jobs } = pageProps;
@@ -131,7 +72,11 @@ export default function Me(pageProps) {
           {I18n.Pages.Me.my_layouts}
         </h1>
 
-        { layouts.length <= 0 && <h2 id="jobSelectTitle">{I18n.Pages.Me.no_layouts}</h2> }
+        { layouts.length <= 0 && (
+          <h2 id="jobSelectTitle">
+            {I18n.Pages.Me.no_layouts}
+          </h2>
+        ) }
       </div>
 
       { layouts.length > 0
@@ -146,6 +91,7 @@ export default function Me(pageProps) {
                       layout={layout}
                       job={job}
                       onDelete={(e) => deleteLayout(e, layout.id)}
+                      className={styles.card}
                     />
                   </li>
                 );
@@ -153,11 +99,13 @@ export default function Me(pageProps) {
 
               { [...Array(maxLayouts - layouts.length)].map((e, i) => (
                 <li key={`placeholder-${i}`}>
-                  <Link href="/">
-                    <a className={styles.card}>
-                      <h4 className={styles.placeholder}>+ New Layout</h4>
-                    </a>
-                  </Link>
+                  <a href="/">
+                    <Card className={styles.card}>
+                      <h4 className={styles.placeholder}>
+                        + New Layout
+                      </h4>
+                    </Card>
+                  </a>
                 </li>
               ))}
             </ul>
