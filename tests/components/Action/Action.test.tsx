541f7ffbf311d2271d60aa14e751a8180c8485e4
Benedicto de Jesus <dejesus.ben@gmail.com>
2023-11-14 12:00:33 -0800

    Test for Action component

---
+++ tests/components/Action/Action.test.tsx
@@ -0,0 +1,12 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import Action from 'components/Action';
+
+describe('Action', () => {
+  it('renders an Action', () => {
+    render(<Action action={{ ID: 1, Name: 'Test' }} />);
+
+    const actionName = screen.getByRole('button', { name: /Test Action/i });
+    expect(actionName).toBeInTheDocument();
+  });
+});
