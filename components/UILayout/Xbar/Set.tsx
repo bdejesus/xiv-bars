e7341e525b07b5c5f103d3413c9d24e027110da0
Benedicto de Jesus <dejesus.ben@gmail.com>
2023-12-21 16:12:15 -0800

    Fixup

--- components/UILayout/Xbar/Set.tsx
+++ components/UILayout/Xbar/Set.tsx
@@ -4,23 +4,18 @@ import styles from './Xbar.module.scss';
 
 interface Props {
   slots: SlotProps[]
-  setID: string
 }
 
-function Set({ slots, setID }: Props) {
+function Set({ slots }: Props) {
   return (
-    <div className={styles.set} id={`${setID}`}>
+    <div className={styles.set}>
       { slots.map((slot) => slot?.id && (
-        <>
-          {/* { console.log(`${setID}-slot-${slot.id}`) } */}
-
-          <Slot
-            id={slot.id}
-            key={`${setID}-slot-${slot.id}`}
-            action={slot.action}
-            className={styles.slot}
-          />
-        </>
+        <Slot
+          id={slot.id}
+          key={`slot-${slot.id}`}
+          action={slot.action}
+          className={styles.slot}
+        />
       ))}
     </div>
   );
