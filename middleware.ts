6aceb3532068833fa6484c395bd5afe759bc6a45
Benedicto de Jesus <dejesus.ben@gmail.com>
2023-12-27 13:22:50 -0800

    Adding CORS

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 function middleware(req: NextRequest) {
-  const currentEnv = process.env.NEXT_PUBLIC_ENV || '';
+  const currentEnv = process.env.NEXT_PUBLIC_ENV || 'development';
+  // retrieve the current response
+  const res = NextResponse.next();
 
   // ssl redirect
   if (['production', 'staging'].includes(currentEnv)
@@ -12,7 +14,16 @@ function middleware(req: NextRequest) {
     );
   }
 
-  return NextResponse.next();
+  // add the CORS headers to the response
+  res.headers.append('Access-Control-Allow-Credentials', 'true');
+  res.headers.append('Access-Control-Allow-Origin', '*');
+  res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT');
+  res.headers.append(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+
+  return res;
 }
 
 export default middleware;
