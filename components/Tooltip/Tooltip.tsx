d67dccd5603a37ad9ecfa38b304ae83ec516590b
Benedicto de Jesus <dejesus.ben@gmail.com>
2024-04-02 12:24:44 -0700

    Fixing tooltip positioning

--- components/Tooltip/Tooltip.tsx
+++ components/Tooltip/Tooltip.tsx
@@ -1,11 +1,14 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { useTooltipState } from './context';
 import Description from './Description';
 import styles from './Tooltip.module.scss';
 
 export function Tooltip() {
+  const tooltipRef = useRef<HTMLDivElement>(null);
+  const contentRef = useRef<HTMLDivElement>(null);
   const [positionStyle, setPositionStyle] = useState({ transform: 'none' });
   const [anchor, setAnchor] = useState('right');
+  const [viewport, setViewport] = useState({ height: 0, width: 0 });
   const { content, position } = useTooltipState();
 
   useEffect(() => {
@@ -13,9 +16,26 @@ export function Tooltip() {
   }, [content]);
 
   useEffect(() => {
+    const { innerHeight: height, innerWidth: width } = window;
+    setViewport({ height, width });
+  }, [window.innerHeight, window.innerWidth]);
+
+  useEffect(() => {
     if (position) {
+      if (tooltipRef.current && contentRef.current) {
+        const {
+          offsetHeight: boxHeight,
+          offsetWidth: boxWidth
+        } = contentRef.current as HTMLDivElement;
+
+        const anchorStyle = [];
+        if (position.x > (viewport.width - boxWidth)) anchorStyle.push('right');
+        if (position.y > (viewport.height - boxHeight)) anchorStyle.push('bottom');
+        tooltipRef.current.dataset.anchor = anchorStyle.join(' ');
+      }
+
       const posStyle = {
-        transform: `translate(${position?.x}px, ${position?.y}px)`
+        transform: `translate(${position?.x}px, ${position?.y}px)`,
       };
       setPositionStyle(posStyle);
     }
@@ -23,17 +43,21 @@ export function Tooltip() {
 
   return (
     <div
+      ref={tooltipRef}
       className={[styles.tooltip, anchor].join(' ')}
       style={positionStyle}
-      aria-hidden={!content?.Name && !content?.Description}
     >
-      {content?.Name && (
-        <>
+      { content?.Name && (
+        <div
+          className={styles.content}
+          ref={contentRef}
+          aria-hidden={!content?.Name && !content?.Description}
+        >
           <h4 className={styles.title}>{content.Name}</h4>
           { content.Description && (
             <Description content={content.Description} />
           )}
-        </>
+        </div>
       )}
     </div>
   );
