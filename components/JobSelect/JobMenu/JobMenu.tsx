1c3dba1e0219cd61572027e030f8785e56e184fe
Benedicto de Jesus <dejesus.ben@gmail.com>
2024-06-12 10:05:48 -0700

    Splits Combat and Crafting ClassJobs into tabs in jobMenu

--- components/JobSelect/JobMenu/JobMenu.tsx
+++ components/JobSelect/JobMenu/JobMenu.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { useAppState } from 'components/App/context';
 import JobsList from '../JobsList';
 import styles from './JobMenu.module.scss';
@@ -7,7 +8,12 @@ interface Props {
 }
 
 export default function JobMenu({ action }:Props) {
+  const tabs = {
+    DowDom: 'DoW/DoM',
+    DohDol: 'DoH/DoL'
+  };
   const { jobs } = useAppState();
+  const [selectedTabId, setSelectedTabId] = useState(Object.keys(tabs)[0]);
 
   if (!jobs) return null;
 
@@ -20,63 +26,88 @@ export default function JobMenu({ action }:Props) {
   const DoH = jobs.filter((job) => job.Discipline === 'DOH');
   const DoL = jobs.filter((job) => job.Discipline === 'DOL');
 
+  function handleTabClick(e:React.MouseEvent<HTMLAnchorElement>) {
+    e.preventDefault();
+    const tabId = e.currentTarget.getAttribute('href')!.slice(1) as keyof typeof tabs;
+    setSelectedTabId(tabId);
+  }
+
   return (
-    <div className={styles.container}>
-      <div className={styles.section}>
-        <h3>DoW/DoM</h3>
+    <>
+      <ul className={styles.tabs}>
+        { Object.entries(tabs).map(([tabId, tabTitle]) => (
+          <li key={tabId}>
+            <a
+              role="button"
+              href={`#${tabId}`}
+              className={[styles.tab, 'button'].join(' ')}
+              onClick={handleTabClick}
+              data-active={selectedTabId === tabId}
+            >
+              {tabTitle}
+            </a>
+          </li>
+        ))}
+      </ul>
 
-        <div className={styles.categories}>
-          <JobsList
-            title="Tank"
-            jobs={tanks}
-            className={styles.jobList}
-            action={action}
-          />
-          <JobsList
-            title="Healer"
-            jobs={healers}
-            className={styles.jobList}
-            action={action}
-          />
-          <JobsList
-            title="Melee DPS"
-            jobs={melee}
-            className={styles.jobList}
-            action={action}
-          />
-          <JobsList
-            title="Range DPS"
-            jobs={range}
-            className={styles.jobList}
-            action={action}
-          />
-          <JobsList
-            title="Magic DPS"
-            jobs={mages}
-            className={styles.jobList}
-            action={action}
-          />
+      <div className={styles.container}>
+
+        <div className={styles.section} id="DowDom" data-active={selectedTabId === 'DowDom'}>
+          <h3>DoW/DoM</h3>
+
+          <div className={styles.categories}>
+            <JobsList
+              title="Tank"
+              jobs={tanks}
+              className={styles.jobList}
+              action={action}
+            />
+            <JobsList
+              title="Healer"
+              jobs={healers}
+              className={styles.jobList}
+              action={action}
+            />
+            <JobsList
+              title="Melee DPS"
+              jobs={melee}
+              className={styles.jobList}
+              action={action}
+            />
+            <JobsList
+              title="Range DPS"
+              jobs={range}
+              className={styles.jobList}
+              action={action}
+            />
+            <JobsList
+              title="Magic DPS"
+              jobs={mages}
+              className={styles.jobList}
+              action={action}
+            />
+          </div>
+        </div>
+
+        <div className={styles.section} id="DohDol" data-active={selectedTabId === 'DohDol'}>
+          <h3>DoH/DoL</h3>
+          <div className={styles.categories}>
+            <JobsList
+              title="Crafter"
+              jobs={DoH}
+              className={styles.jobList}
+              action={action}
+            />
+            <JobsList
+              title="Gatherer"
+              jobs={DoL}
+              className={styles.jobList}
+              action={action}
+            />
+          </div>
         </div>
       </div>
-
-      <div className={styles.section}>
-        <h3>DoH/DoL</h3>
-        <div className={styles.categories}>
-          <JobsList
-            title="Crafter"
-            jobs={DoH}
-            className={styles.jobList}
-            action={action}
-          />
-          <JobsList
-            title="Gatherer"
-            jobs={DoL}
-            className={styles.jobList}
-            action={action}
-          />
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
 
