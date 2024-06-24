fb8ac949e9018254f73dcebcebb0d5ff699b44f6
Benedicto de Jesus <dejesus.ben@gmail.com>
2024-06-24 09:55:51 -0700

    Fixup to tests

--- tests/lib/utils/time.test.ts
+++ tests/lib/utils/time.test.ts
@@ -5,14 +5,14 @@ const dateString = "2024-06-14T00:00:00Z";
 describe('formatDateString', () => {
   it('returns a short formatted date', () => {
     const formattedDate = formatDateString(dateString);
-    expect(formattedDate).toBe('Thu, Jun 13, 2024');
+    expect(formattedDate).toBe('Fri, Jun 14, 2024');
   });
 });
 
 describe('formatDateStringLong', () => {
   it('returns a long formatted date', () => {
     const formattedDate = formatDateStringLong(dateString);
-    expect(formattedDate).toBe('Thursday, June 13, 2024')
+    expect(formattedDate).toBe('Friday, June 14, 2024')
   });
 });
 
