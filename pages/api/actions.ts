a7a835e1b4eb6dac3980030c4d895190242aaf81
Benedicto de Jesus <dejesus.ben@gmail.com>
2024-06-27 10:53:19 -0700

    Fixup

--- pages/api/actions.ts
+++ pages/api/actions.ts
@@ -4,12 +4,9 @@ import Jobs from 'apiData/Jobs.json';
 
 export default async function actionsHandler(req: NextApiRequest, res: NextApiResponse) {
   try {
-    const jobIds = Jobs.map(({ Abbr }) => Abbr);
     const params = req.query;
-
-    if (!jobIds.includes(params.job as string)) throw new Error('Not Found');
-
-    const file = `${process.cwd()}/.apiData/JobActions/${params.job}.json`;
+    const jobId = Jobs.map(({ Abbr }) => Abbr).find((id) => id === params.job);
+    const file = `${process.cwd()}/.apiData/JobActions/${jobId}.json`;
 
     fs.readFile(file, 'utf8', (err, data) => {
       if (err) {
