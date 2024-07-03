dacc49a770a33fa0a93be23a0ef75771a4f5173c
Benedicto de Jesus <dejesus.ben@gmail.com>
2024-07-03 14:18:01 -0700

    Fixup to ProfileImage props

--- components/User/ProfileImage/ProfileImage.tsx
+++ components/User/ProfileImage/ProfileImage.tsx
@@ -3,6 +3,17 @@ import Link from 'next/link';
 
 import styles from './ProfileImage.module.scss';
 
+interface ImageProps {
+  src?: string,
+  alt: string
+}
+
+function Image({ src = undefined, alt }:ImageProps) {
+  return src
+    ? <img src={src} alt={alt} itemProp="image" />
+    : <img src="/icons/favicon-96x96.png" className={styles.placeholder} alt={alt} />;
+}
+
 interface ProfileImageProps {
   title: string,
   src?: string,
@@ -16,18 +27,14 @@ export default function ProfileImage({
   href,
   className = ''
 }:ProfileImageProps) {
-  const Image = () => src
-    ? <img src={src} alt={title} itemProp="image" />
-    : <img src={'/icons/favicon-96x96.png'} className={styles.placeholder} alt={title} />;
-
   return (
     href ? (
       <Link href={href} className={[styles.wrapper, className].join(' ')}>
-        <Image />
+        <Image src={src} alt={title} />
       </Link>
     ) : (
       <div className={[styles.wrapper, className].join(' ')}>
-        <Image />
+        <Image src={src} alt={title} />
       </div>
     )
   );
