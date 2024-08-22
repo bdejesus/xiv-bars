4b6708a0f89be88f3fca2c9e787eb00912e27960
Benedicto de Jesus <dejesus.ben@gmail.com>
2024-08-21 23:29:12 -0700

    Fixup

--- components/AdUnit/AdUnit.tsx
+++ components/AdUnit/AdUnit.tsx
@@ -4,13 +4,14 @@ import styles from './AdUnit.module.scss';
 interface AdUnitProps {
   width?: number,
   height?: number,
+  className?: string,
   format?: 'display' | 'feed' | 'fixed-square'
 }
 
 function Display() {
   return (
     <ins
-      className={`${styles.container} adsbygoogle`}
+      className="adsbygoogle"
       style={{ display: 'block' }}
       data-ad-client={process.env.NEXT_PUBLIC_GOOGLE_ADSENSE}
       data-ad-slot="2483095747"
@@ -23,7 +24,7 @@ function Display() {
 function FixedSize({ height = 360, width = 368 }:{height?: number, width?: number}) {
   return (
     <ins
-      className={`${styles.container} ${styles.inFeed} adsbygoogle`}
+      className="adsbygoogle"
       style={{ display: 'inline-block', width: `${width}px`, height: `${height}px` }}
       data-ad-client={process.env.NEXT_PUBLIC_GOOGLE_ADSENSE}
       data-ad-slot="9103811582"
@@ -34,22 +35,30 @@ function FixedSize({ height = 360, width = 368 }:{height?: number, width?: numbe
 export default function AdUnit({
   width,
   height,
+  className = '',
   format = 'display'
 }:AdUnitProps) {
   const enabled = !!process.env.NEXT_PUBLIC_GOOGLE_ADSENSE;
 
   useEffect(() => {
-    if ((window as any).adsbygoogle) { // eslint-disable-line
-      try {
-        (window as any).adsbygoogle.push({}); // eslint-disable-line
-      } catch (e) {
-        console.error('AdSense error:', e);
-      }
+    if (typeof window !== 'undefined' && enabled) {
+      ((window as any).adsbygoogle = (window as any).adsbygoogle || []).push({}); // eslint-disable-line
     }
   }, []);
 
   if (!enabled) return null;
 
-  if (format === 'fixed-square') return <FixedSize height={height} width={width} />;
-  return <Display />;
+  return (
+    <div
+      className={`${styles.container} ${className}`}
+      style={{
+        width: width ? `${width}px` : 'unset',
+        height: height ? `${height}px` : 'unset'
+      }}
+    >
+      { format === 'fixed-square'
+        ? <FixedSize height={height} width={width} />
+        : <Display />}
+    </div>
+  );
 }
