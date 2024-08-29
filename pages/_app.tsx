9a0069e72f03fe37ee4e521a3db58cd0b8825daa
Benedicto de Jesus <dejesus.ben@gmail.com>
2024-08-28 21:19:58 -0700

    Fixup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -84,7 +84,7 @@ function App({ Component, pageProps }: AppProps) {
           async
           src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE}`}
           crossOrigin="anonymous"
-          strategy="afterInteractive"
+          strategy="lazyOnload"
         />
       )}
 
