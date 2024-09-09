2ac389e298c8ed10c2bff717bc4c0e8edb2d8f5a
Benedicto de Jesus <dejesus.ben@gmail.com>
2024-09-09 15:51:30 -0700

    Cleaning up user page updates (#308)

--- pages/user/[userId].tsx
+++ pages/user/[userId].tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useTranslation } from 'next-i18next';
 import db, { serializeDates, layoutsQuery } from 'lib/db';
