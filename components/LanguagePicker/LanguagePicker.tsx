ed0a6e9aa0897e603d115258e2598463140dd9ce
Benedicto de Jesus <dejesus.ben@gmail.com>
2025-01-10 12:37:16 -0800

    Cleaning up language picker

--- components/LanguagePicker/LanguagePicker.tsx
+++ components/LanguagePicker/LanguagePicker.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import Icon from 'components/Icon';
+import { languages } from 'lib/utils/i18n.mjs';
 import styles from './LanguagePicker.module.scss';
 
 export default function LanguagePicker() {
@@ -12,45 +13,37 @@ export default function LanguagePicker() {
     setShowMenu(false);
   }, [router.locale]);
 
-  function handleKeyPress(e:React.KeyboardEvent<HTMLElement>) {
+  function handleKeyUp(e:React.KeyboardEvent<HTMLElement>) {
     if (e.code === 'Enter') setShowMenu(!showMenu);
+    if (e.code === 'Escape') setShowMenu(false);
   }
 
   return (
-    <div className={styles.container}>
+    <div
+      className={styles.container}
+      onMouseLeave={() => setShowMenu(false)}
+    >
       <button
         type="button"
         className={`button btn-default btn-menu ${styles.container}`}
         onClick={() => setShowMenu(!showMenu)}
-        onKeyUp={handleKeyPress}
+        onKeyUp={handleKeyUp}
+        title={languages[router.locale as keyof typeof languages]}
       >
         <Icon id="locale" alt={router.locale!} />
-        <div className="btn-label">
+        <div className={styles.locale}>
           {router.locale}
         </div>
       </button>
 
       <ul className={styles.menu} aria-hidden={!showMenu}>
-        <li>
-          <Link href={decodeURIComponent(router.asPath)} locale="en">
-            EN - English
-          </Link>
-        </li>
-        <li>
-          <Link href={decodeURIComponent(router.asPath)} locale="ja">
-            JA - 日本語
-          </Link>
-        </li>
-        <li>
-          <Link href={decodeURIComponent(router.asPath)} locale="de">
-            DE - Deutsch
-          </Link>
-        </li>
-        <li>
-          <Link href={decodeURIComponent(router.asPath)} locale="fr">
-            FR - Français
-          </Link>
-        </li>
+        { Object.entries(languages).map(([locale, label]) => (
+          <li key={`locale-option-${locale}`}>
+            <Link href={decodeURIComponent(router.asPath)} locale={locale}>
+              <span className={styles.locale}>{locale}</span>&nbsp;- {label}
+            </Link>
+          </li>
+        )) }
       </ul>
     </div>
   );
